f1d34bb0624b7cf2ac2cb18d933500f039c26ec1
Azmi Muwahid <azmimuwahid@gmail.com>
2022-02-14 21:42:57 +0700

    change ssl

--- _helpers/db.js
+++ _helpers/db.js
@@ -33,7 +33,7 @@ async function initialize() {
   // init models and add them to the exported db object
   db.User = require("../users/user.model")(sequelize);
   db.Tree = require("../trees/tree.model")(sequelize);
-  db.Log = require("../Logs/log.model")(sequelize);
+  db.Log = require("../logs/log.model")(sequelize);
 
   // sync all models with database
   await sequelize.sync({ alter: true });
