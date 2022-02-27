f8e8a7d93292588d0b8feff2b66d4ac62c2644d2
Azmi Muwahid <azmimuwahid@gmail.com>
2022-02-27 17:22:44 +0700

    add auth

--- server.mjs
+++ server.mjs
@@ -43,7 +43,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
     credentials: true,
   })
