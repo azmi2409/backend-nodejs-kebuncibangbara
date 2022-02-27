8575468e48ce61197649ad4d7ee6de62190c0a74
Azmi Muwahid <azmimuwahid@gmail.com>
2022-02-27 13:02:59 +0700

    add auth

--- server.mjs
+++ server.mjs
@@ -58,7 +58,7 @@ app.get("/getJWT", verifySession(), async (req, res) => {
 
   res.json({ token: jwt });
 });
-app.use("/trees", verifySession(), treesRouter);
+app.use("/trees", treesRouter);
 app.use("/logs", logsRouter);
 
 // global error handler
